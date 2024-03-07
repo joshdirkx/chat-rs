3dbe6c4acd5c4e53674bbf80d360b4291aec1e32
Josh Dirkx <joshdirkx@gmail.com>
2024-03-07 15:57:18 -0800

    add deps and build dep

--- backend/build.rs
+++ backend/build.rs
@@ -1,5 +1,7 @@
-fn main() {
-    tonic_build::configure()
-        .compile(&["proto/messaging.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("Failed to compile proto files: {}", e));
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tonic_build::compile_protos("proto/messaging.proto")?;
+
+    Ok(())
 }
